69318c81f22253993d152d9b6f3f82b61533548e
Justin Lam <justin.kh.lam@gmail.com>
2022-10-13 13:52:59 -0400

    import react-router dom into App.js

--- api/src/App.js
+++ api/src/App.js
@@ -1,7 +1,19 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
