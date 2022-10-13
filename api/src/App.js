405fb73fedb8356dc9e344e7afb41d4d1509caed
Justin Lam <justin.kh.lam@gmail.com>
2022-10-13 13:57:41 -0400

    import home/login page components

--- api/src/App.js
+++ api/src/App.js
@@ -2,16 +2,22 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import "./App.css";
 
 // Components
+import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home</div>,
+    element: <Home />,
   },
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
