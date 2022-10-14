dd496def76eb5efdbdb99e47c8ab82d5af5c5587
Justin Lam <justin.kh.lam@gmail.com>
2022-10-14 12:22:40 -0400

    add in h1 and form elements for login component

--- api/src/pages/Login.jsx
+++ api/src/pages/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
