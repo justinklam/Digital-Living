0bc8b95745e0c035615dd18868f8db380f9c1709
Justin Lam <justin.kh.lam@gmail.com>
2022-10-16 00:28:01 -0400

    import image assets into Navbar

--- api/src/components/Navbar.jsx
+++ api/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Image assets
+import Logo from "../img/logo.png";
+import Logo2 from "../img/logo.svg";
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">Logo</div>
+        <div className="logo">
+          <img src="" alt="" />
+        </div>
         <div className="links">Links</div>
       </div>
     </div>
