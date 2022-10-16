611a5d7bac978004fb7ea7e8482996250c066d86
Justin Lam <justin.kh.lam@gmail.com>
2022-10-16 00:17:31 -0400

    add some baseline styling for app.js and container. add base styling to Navbar element

--- api/src/components/Navbar.jsx
+++ api/src/components/Navbar.jsx
@@ -5,7 +5,8 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo"></div>
+        <div className="logo">Logo</div>
+        <div className="links">Links</div>
       </div>
     </div>
   );
