3f056f17319fb810b384056c3e64c387ee00c468
Justin Lam <justin.kh.lam@gmail.com>
2022-10-15 23:30:47 -0400

    add in elements for register page

--- api/src/pages/Register.jsx
+++ api/src/pages/Register.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Register</button>
+        <p>Error!</p>
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
