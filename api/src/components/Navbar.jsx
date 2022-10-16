c29457870aaee9767928b8c72092192a0398e3b4
Justin Lam <justin.kh.lam@gmail.com>
2022-10-16 00:11:33 -0400

    add in react-router-dom link. add in additional elements

--- api/src/components/Navbar.jsx
+++ api/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
