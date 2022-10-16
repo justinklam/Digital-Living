91ed1c41ef39f02cca8b0bb7d6ab601a96f46c87
Justin Lam <justin.kh.lam@gmail.com>
2022-10-16 00:33:27 -0400

    add in additional links to other categories

--- api/src/components/Navbar.jsx
+++ api/src/components/Navbar.jsx
@@ -16,6 +16,18 @@ const Navbar = () => {
           <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
         </div>
       </div>
     </div>
