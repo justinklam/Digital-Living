4f20aa745c6ba9b1552b538ef76039232dce67f5
Justin Lam <justin.kh.lam@gmail.com>
2022-10-17 10:59:17 -0400

    set navbar logo to redirect to home

--- api/src/components/Navbar.jsx
+++ api/src/components/Navbar.jsx
@@ -10,7 +10,9 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt="logo" />
+          <Link className="link" to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
         </div>
         <div className="links">
           <Link className="link" to="/?cat=art">
