b7e434aeb8fa8af9e129867c28a073be2435eb52
Justin Lam <justin.kh.lam@gmail.com>
2022-10-17 10:55:31 -0400

    add in logo and elements into footer

--- api/src/components/Footer.jsx
+++ api/src/components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// Image assets
+import Logo from "../img/logo.png";
+// import Logo2 from "../img/logo.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="logo" />
+      <span>
+        Made by{" "}
+        <a href="https://justinklam.netlify.app/">
+          <b>Justin</b>
+        </a>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
