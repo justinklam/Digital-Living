c9e7d193af9fccaa38d17f3e0fc0d796cb69bec9
Justin Lam <justin.kh.lam@gmail.com>
2022-10-17 10:48:15 -0400

    add in styling for navbar h6 and span. add in classname for link

--- api/src/components/Navbar.jsx
+++ api/src/components/Navbar.jsx
@@ -31,7 +31,9 @@ const Navbar = () => {
           <span>User 1</span>
           <span>Logout</span>
           <span className="write">
-            <Link to="/write">Write</Link>
+            <Link className="link" to="/write">
+              Write
+            </Link>
           </span>
         </div>
       </div>
