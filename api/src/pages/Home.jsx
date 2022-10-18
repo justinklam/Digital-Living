85c4c3fc3bfbb4d6e026a5c7d2970dc9b444b58d
Justin Lam <justin.kh.lam@gmail.com>
2022-10-18 14:43:20 -0400

    shift desc and button outside of Link. correct .map with ( from {

--- api/src/pages/Home.jsx
+++ api/src/pages/Home.jsx
@@ -26,16 +26,18 @@ const Home = () => {
   return (
     <div className="home">
       <div className="posts">
-        {posts.map((post) => {
+        {posts.map((post) => (
           <div className="post" key={post.id}>
             <img src={post.img} alt="img" />
             <div className="content">
-              <Link to={`/post/${post.id}`}>
+              <Link className="link" to={`/post/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
+              <p>{post.desc}</p>
+              <button>Read More</button>
             </div>
-          </div>;
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
