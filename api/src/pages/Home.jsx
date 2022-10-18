e4ef42e959f832c1321a8e5e06f28d2843e6c46d
Justin Lam <justin.kh.lam@gmail.com>
2022-10-18 14:37:13 -0400

    add in placeholder dummy data

--- api/src/pages/Home.jsx
+++ api/src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const posts = [
+    {
+      id: 1,
+      title: "title1",
+      desc: "desc1",
+      img: "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      title: "title2",
+      desc: "desc2",
+      img: "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      title: "title3",
+      desc: "desc3",
+      img: "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+
+  return <div className="home">Home</div>;
 };
 
 export default Home;
