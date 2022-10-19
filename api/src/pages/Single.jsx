ced4ad910f1cae4bd6fe82f66ce07c577b489d69
Justin Lam <justin.kh.lam@gmail.com>
2022-10-19 12:28:59 -0400

    add in single component page elements and base styling

--- api/src/pages/Single.jsx
+++ api/src/pages/Single.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">Content</div>
+      <div className="menu">Menu</div>
+    </div>
+  );
 };
 
 export default Single;
