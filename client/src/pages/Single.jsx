c5b46dda7238a46743f6c39225e67f41434bddcb
Justin Lam <justin.kh.lam@gmail.com>
2022-10-20 00:41:19 -0400

    add in elements with placeholder dummy data

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -3,7 +3,22 @@ import React from "react";
 const Single = () => {
   return (
     <div className="single">
-      <div className="content">Content</div>
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="img"
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="img2"
+          />
+        </div>
+        <div className="info">
+          <span>user1</span>
+          <p>Posted 2 days ago</p>
+        </div>
+      </div>
       <div className="menu">Menu</div>
     </div>
   );
