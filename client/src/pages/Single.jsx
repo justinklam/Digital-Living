e7e0f3b91d60e2cdb9461f1121689ea91af5636d
Justin Lam <justin.kh.lam@gmail.com>
2022-10-20 00:46:43 -0400

    shift info section under user

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -13,10 +13,10 @@ const Single = () => {
             src="https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="img2"
           />
-        </div>
-        <div className="info">
-          <span>user1</span>
-          <p>Posted 2 days ago</p>
+          <div className="info">
+            <span>user1</span>
+            <p>Posted 2 days ago</p>
+          </div>
         </div>
       </div>
       <div className="menu">Menu</div>
