ddebcf32f09ad35b3a0fc0e2d6e8efd926c0a838
Justin Lam <justin.kh.lam@gmail.com>
2022-10-21 12:06:07 -0400

    add elements for title and paragraph body

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -29,6 +29,32 @@ const Single = () => {
             <img src={deleteIcon} alt="delete" />
           </div>
         </div>
+        <h1>This is the title</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut leo a
+          lectus rutrum ornare ac laoreet lorem. Cras sit amet purus sed arcu
+          tempus iaculis. Fusce consequat eros ex, sit amet pretium arcu
+          sagittis id. Interdum et malesuada fames ac ante ipsum primis in
+          faucibus. Pellentesque vel commodo lacus, aliquet interdum metus.
+          Aliquam condimentum leo et vestibulum vehicula. Vivamus lobortis
+          finibus augue a pellentesque. Fusce quis ligula nulla. In hac
+          habitasse platea dictumst. Integer egestas ipsum in nisl suscipit
+          semper. Nam eu rhoncus massa. Fusce a imperdiet lacus. In felis
+          tellus, feugiat tempor orci quis, condimentum posuere leo. Etiam at
+          commodo dolor, sed porttitor est. Vestibulum sit amet purus sit amet
+          quam rutrum fermentum. Cras eget sagittis ipsum. Morbi euismod viverra
+          risus, at ultrices dolor fermentum et. Mauris eu fermentum lacus. Nam
+          pulvinar porttitor arcu id feugiat. Quisque libero arcu, finibus ac
+          mauris id, convallis dapibus metus. Donec eu nisi sed mauris ornare
+          cursus. Cras vel urna diam. Sed lobortis accumsan finibus. Etiam
+          bibendum consequat lorem, eget congue ligula maximus non. Curabitur
+          mattis congue nulla at molestie. Mauris varius justo risus, non
+          viverra nulla scelerisque sed. Vestibulum placerat id nibh in maximus.
+          Quisque dui nulla, rutrum vel hendrerit sit amet, facilisis et sapien.
+          Pellentesque mi nisi, egestas vel dolor non, volutpat consequat ex.
+          Maecenas ac tempor neque, et venenatis magna. Interdum et malesuada
+          fames ac ante ipsum primis in faucibus.
+        </p>
       </div>
       <div className="menu">Menu</div>
     </div>
