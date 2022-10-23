7f3a3763e484322ccaee00a13243cd2729d46f6c
Justin Lam <justin.kh.lam@gmail.com>
2022-10-23 00:47:45 -0400

    import in React-quill. add in value states

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+// React Quill
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 const Write = () => {
+  const [value, setValue] = useState("");
+
   return (
     <div className="writePost">
       <div className="content">
         <input type="text" placeHolder="Title" />
+        <div className="editorContainer">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
       </div>
       <div className="menu">
         <div className="item">Item 1</div>
