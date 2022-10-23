d2676aaea9a4eb91b9e600cc8624839d0286cf68
Justin Lam <justin.kh.lam@gmail.com>
2022-10-23 00:41:13 -0400

    add base elements into write component

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Write = () => {
-  return <div>Write</div>;
+  return (
+    <div className="write">
+      <div className="content">Content</div>
+      <div className="menu">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Write;
