118d8e02def0bbf281ec7a6e97e4f46080f2b311
Justin Lam <justin.kh.lam@gmail.com>
2022-10-23 00:46:01 -0400

    install react-quill

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const Write = () => {
   return (
     <div className="writePost">
-      <div className="content">Content</div>
+      <div className="content">
+        <input type="text" placeHolder="Title" />
+      </div>
       <div className="menu">
         <div className="item">Item 1</div>
         <div className="item">Item 2</div>
