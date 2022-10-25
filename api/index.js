b08076b1461cc7edebcfb3adcac7f7bdf83faaad
Justin Lam <justin.kh.lam@gmail.com>
2022-10-25 13:17:03 -0400

    add in dotenv config. add in port from .env file. initialize server

--- api/index.js
+++ api/index.js
@@ -3,5 +3,10 @@ import express from "express";
 import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(process.env);
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server connected at ${port}! 🐅`);
+});
