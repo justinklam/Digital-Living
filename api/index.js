6770270277b18fda68cb507f979814e17460fe0e
Justin Lam <justin.kh.lam@gmail.com>
2022-10-26 00:32:49 -0400

    import and configure cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server connected at ${port}! 🐅`);
