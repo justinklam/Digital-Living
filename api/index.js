02f47529326f3992d009bb8d9aba71fa15cb7acb
Justin Lam <justin.kh.lam@gmail.com>
2022-10-27 13:51:58 -0400

    add in posts route into index.js

--- api/index.js
+++ api/index.js
@@ -3,16 +3,23 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+// Routes
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+
+// Use Routes
+app.use("/api/posts", postRoutes);
 
 app.listen(port, () => {
   console.log(`Server connected at ${port}! 🐅`);
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -2,6 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
+// Get
 router.get("/", (req, res) => {
   res.json("Post Route");
 });
