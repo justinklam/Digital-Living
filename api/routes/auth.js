a294d61031df9f71c434b71648fa16f5d7723034
Justin Lam <justin.kh.lam@gmail.com>
2022-10-27 15:20:15 -0400

    import in controller logic

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,16 @@
 import express from "express";
+// Controller
+import { register, login, logout } from "../controllers/auth";
 
 const router = express.Router();
 
-// Get
-router.get("/", (req, res) => {
-  res.json("Auth Get Route");
-});
+// Register
+router.post("/register", register);
+
+// Login
+router.post("/login", login);
+
+// Logout
+router.post("/logout", logout);
 
 export default router;
