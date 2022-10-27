2fe61a2762810c560fb6b41bddb1fc31ed76cd6d
Justin Lam <justin.kh.lam@gmail.com>
2022-10-27 15:19:31 -0400

    initialize auth controller

---
+++ api/controllers/auth.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  console.log("register");
+};
+
+export const login = (req, res) => {
+  console.log("login");
+};
+
+export const logout = (req, res) => {
+  console.log("logout");
+};
