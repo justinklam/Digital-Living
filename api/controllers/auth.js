279aa4fa4d5993baa05e109199645026e322e92f
Justin Lam <justin.kh.lam@gmail.com>
2022-10-28 00:54:31 -0400

    add in logic to check user

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,7 +3,15 @@ import { db } from "../db";
 export const register = (req, res) => {
   // CHECK EXISTING USER
   const q = "SELECT * FROM users WHERE email = ? OR username = ?";
-  db.query(q, [req.body.email, req.body.username]);
+
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    // fail route
+    if (err) return res.json("Register Error:", err);
+
+    // success route
+    // if user is found - data.length exists
+    if (data.length) return res.status(409).json("User account exists!");
+  });
 };
 
 export const login = (req, res) => {
