aa84af616a477071ced2abc091c901bbfe2c514e
Justin Lam <justin.kh.lam@gmail.com>
2022-10-28 00:57:48 -0400

    import bcryptjs

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,7 @@
-import { db } from "../db";
+import { db } from "../db.js";
+import bcrypt from "bcryptjs";
 
+// REGISTER
 export const register = (req, res) => {
   // CHECK EXISTING USER
   const q = "SELECT * FROM users WHERE email = ? OR username = ?";
@@ -7,17 +9,22 @@ export const register = (req, res) => {
   db.query(q, [req.body.email, req.body.username], (err, data) => {
     // fail route
     if (err) return res.json("Register Error:", err);
-
     // success route
     // if user is found - data.length exists
     if (data.length) return res.status(409).json("User account exists!");
+
+    // Hash the password and create a user
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
   });
 };
 
+// LOGIN
 export const login = (req, res) => {
   console.log("login");
 };
 
+// LOGOUT
 export const logout = (req, res) => {
   console.log("logout");
 };
