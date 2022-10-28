35665f48af407310a27f6f2a0d07633391564a71
Justin Lam <justin.kh.lam@gmail.com>
2022-10-28 01:00:10 -0400

    add in query to register a new user

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -8,7 +8,7 @@ export const register = (req, res) => {
 
   db.query(q, [req.body.email, req.body.username], (err, data) => {
     // fail route
-    if (err) return res.json("Register Error:", err);
+    if (err) return res.json("Registration Error:", err);
     // success route
     // if user is found - data.length exists
     if (data.length) return res.status(409).json("User account exists!");
@@ -16,6 +16,9 @@ export const register = (req, res) => {
     // Hash the password and create a user
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users (`username`, `email`, `password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash];
   });
 };
 
