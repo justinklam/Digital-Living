6c1fa506b9944e7b3e17592ae06c8a51757920d3
Justin Lam <justin.kh.lam@gmail.com>
2022-10-28 00:51:57 -0400

    import in db. add in query to check user

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,9 @@
+import { db } from "../db";
+
 export const register = (req, res) => {
-  console.log("register");
+  // CHECK EXISTING USER
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+  db.query(q, [req.body.email, req.body.username]);
 };
 
 export const login = (req, res) => {
