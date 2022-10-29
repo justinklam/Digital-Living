5005e03ef6ab7633e918753f11a2340b22b561a9
Justin Lam <justin.kh.lam@gmail.com>
2022-10-29 12:46:12 -0400

    add in res.status on return messags

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -8,7 +8,8 @@ export const register = (req, res) => {
 
   db.query(q, [req.body.email, req.body.username], (err, data) => {
     // fail route
-    if (err) return res.json("Registration Error:", err);
+    if (err) return res.status(500).json("Registration Error:", err);
+
     // success route
     // if user is found - data.length exists
     if (data.length) return res.status(409).json("User account exists!");
@@ -17,11 +18,12 @@ export const register = (req, res) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const q = "INSERT INTO users (`username`, `email`, `password`) VALUES (?)";
+    const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
+
     const values = [req.body.username, req.body.email, hash];
 
     db.query(q, [values], (err, data) => {
-      if (err) return res.json("Registration Error:", err);
+      if (err) return res.status(500).json("Registration Error:", err);
 
       return res.status(200).json("User has been registered!");
     });
