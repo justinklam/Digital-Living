e7df9e9859aceb4d32b99f4db448456bb12a9cd0
Justin Lam <justin.kh.lam@gmail.com>
2022-10-30 00:41:46 -0400

    add in input/error useStates, add in handleChange/handleSubmit handlers for input and axios request

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8080/api/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Login</h1>
