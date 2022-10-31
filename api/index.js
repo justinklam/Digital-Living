e6fb6c7a0490a242af154639b1bf930870fb6cd5
Justin Lam <justin.kh.lam@gmail.com>
2022-10-31 11:32:53 -0400

    install and import in cookie parser

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // Routes
 import authRoutes from "./routes/auth.js";
@@ -14,6 +15,7 @@ dotenv.config();
 const port = parseInt(process.env.PORT) || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use(
   cors({
