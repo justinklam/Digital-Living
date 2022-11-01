3bac9df21e1890af6506473d9e32a5c1c68ec2a7
Justin Lam <justin.kh.lam@gmail.com>
2022-11-01 00:38:26 -0400

    shift login axios call to authContext

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import axios from "axios";
 
 export const AuthContext = createContext();
 
@@ -6,4 +7,15 @@ export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
     JSON.parse(localStorage.getItem("user") || null)
   );
+
+  const login = async (inputs) => {
+    const res = await axios.post(
+      "http://localhost:8080/api/auth/login",
+      inputs,
+      {
+        withCredentials: true,
+      }
+    );
+    setCurrentUser(res.data);
+  };
 };
