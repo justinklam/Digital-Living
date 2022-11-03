46100c64c7b6a7e22f92a7ba9593af7ef22e2fc9
Justin Lam <justin.kh.lam@gmail.com>
2022-11-03 00:51:05 -0400

    import in db. add in db query for getPosts

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,8 +1,16 @@
+import { db } from "../db.js";
+
 export const getPosts = (req, res) => {
   // req.query.cat takes everything after the URL address cat
   const q = req.query.cat
     ? "SELECT * FROM posts WHERE cat=?"
     : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
+
+    return res.status(200).json(data);
+  });
 };
 
 export const getPost = (req, res) => {
