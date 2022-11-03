cf01c4ecf838b9719d1239e9b076612b346e48b9
Justin Lam <justin.kh.lam@gmail.com>
2022-11-03 00:37:26 -0400

    update base controller functions for routes

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,1 +1,19 @@
-export const addPost = (req, res) => {};
+export const getPosts = (req, res) => {
+  res.json("fromController");
+};
+
+export const getPost = (req, res) => {
+  res.json("fromController");
+};
+
+export const addPost = (req, res) => {
+  res.json("fromController");
+};
+
+export const deletePost = (req, res) => {
+  res.json("fromController");
+};
+
+export const updatePost = (req, res) => {
+  res.json("fromController");
+};
