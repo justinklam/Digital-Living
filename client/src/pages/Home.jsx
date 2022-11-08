7b7083565084a445aab25cd28591b8e1d4f941d8
Justin Lam <justin.kh.lam@gmail.com>
2022-11-07 20:45:12 -0500

    set readMore to direct to post instead of title

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -52,11 +52,11 @@ const Home = () => {
               <img src={post.img} alt="img" />
             </div>
             <div className="content">
-              <Link className="link" to={`/post/${post.id}`}>
-                <h1>{post.title}</h1>
-              </Link>
+              <h1>{post.title}</h1>
               <p>{post.desc}</p>
-              <button>Read More</button>
+              <Link className="link" to={`/post/${post.id}`}>
+                <button>Read More</button>
+              </Link>
             </div>
           </div>
         ))}
