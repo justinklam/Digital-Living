71b2945ac6c07b09917ddd25705a42b762fdd02b
Justin Lam <justin.kh.lam@gmail.com>
2022-11-11 11:41:08 -0500

    add in logic to past over postId and userId into update query

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -82,12 +82,13 @@ export const updatePost = (req, res) => {
   jwt.verify(token, process.env.JWT_KEY, (err, userInfo) => {
     if (err) return res.status(403).json("Token is invalid!");
 
+    const postId = req.params.id;
     const q =
       "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=?  WHERE `id` = ? AND `uid` = ?";
 
     const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
 
-    db.query(q, [...values], (err, data) => {
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
 
       return res.json("Post has been updated!");
