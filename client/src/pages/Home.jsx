6d507fb056ab654da85b47d9909ed52f33ffaeaf
Justin Lam <justin.kh.lam@gmail.com>
2022-11-12 13:34:35 -0500

    remove unused dummy data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,27 +22,6 @@ const Home = () => {
     fetchData();
   }, [category]);
 
-  // const posts = [
-  //   {
-  //     id: 1,
-  //     title: "Title 1",
-  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis pulvinar erat. In leo velit, blandit sed volutpat vel, dapibus et massa. Cras consectetur, diam vel volutpat auctor, libero ex eleifend purus, eget congue purus purus vel elit. Donec eu lorem rutrum, tempus enim feugiat, hendrerit tellus. Phasellus ac iaculis felis. Vivamus molestie nunc sapien, eget volutpat ante pulvinar ut. Integer efficitur suscipit ex ut laoreet. Duis a rhoncus magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis sem vitae nunc elementum posuere sit amet vel libero. Donec lorem nisi, mollis vel interdum sed, auctor id magna. Etiam venenatis nibh sed tortor lacinia dapibus",
-  //     img: "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Title 2",
-  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis pulvinar erat. In leo velit, blandit sed volutpat vel, dapibus et massa. Cras consectetur, diam vel volutpat auctor, libero ex eleifend purus, eget congue purus purus vel elit. Donec eu lorem rutrum, tempus enim feugiat, hendrerit tellus. Phasellus ac iaculis felis. Vivamus molestie nunc sapien, eget volutpat ante pulvinar ut. Integer efficitur suscipit ex ut laoreet. Duis a rhoncus magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis sem vitae nunc elementum posuere sit amet vel libero. Donec lorem nisi, mollis vel interdum sed, auctor id magna. Etiam venenatis nibh sed tortor lacinia dapibus",
-  //     img: "https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Title 3",
-  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis pulvinar erat. In leo velit, blandit sed volutpat vel, dapibus et massa. Cras consectetur, diam vel volutpat auctor, libero ex eleifend purus, eget congue purus purus vel elit. Donec eu lorem rutrum, tempus enim feugiat, hendrerit tellus. Phasellus ac iaculis felis. Vivamus molestie nunc sapien, eget volutpat ante pulvinar ut. Integer efficitur suscipit ex ut laoreet. Duis a rhoncus magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis sem vitae nunc elementum posuere sit amet vel libero. Donec lorem nisi, mollis vel interdum sed, auctor id magna. Etiam venenatis nibh sed tortor lacinia dapibus",
-  //     img: "https://images.pexels.com/photos/208984/pexels-photo-208984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //   },
-  // ];
-
   // to remove html tags on text body
   const getText = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html");
