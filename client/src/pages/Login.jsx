df49858e9e06a92453dd291404a7eb4665aced8f
Justin Lam <justin.kh.lam@gmail.com>
2022-11-13 01:20:44 -0500

    code cleanup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import axios from "axios";
 import { AuthContext } from "../context/authContext";
 
 // Components
